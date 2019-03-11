428576dfd6bc27ff9b5230a30e263218f6dded3c
unknown <guandeyu1@cn.net.ntes>
2019-03-11 19:15:53 +0800

    Plan

--- NN/PFNN.py
+++ NN/PFNN.py
@@ -1,61 +1,67 @@
 import tensorflow as tf
 import numpy as np
 
-dataset = np.load('./data/database.npz')
-X = dataset['Xun'].astype(np.float)
-Y = dataset['Yun'].astype(np.float)
-P = dataset['Pun'].astype(np.float)
+# Config
+EPOCH = 10
+BATCH_SIZE = 64
+DATASET_PATH = "C:\\Users\\guandeyu1\\Documents\\Code\\PFNN\\"
 
-print(X.shape, ',', Y.shape)
+# dataset = np.load(DATASET_PATH + 'database.npz')
+# X = dataset['Xun'].astype(np.float)
+# Y = dataset['Yun'].astype(np.float)
+# P = dataset['Pun'].astype(np.float)
 
-Xmean, Xstd = X.mean(axis=0), X.std(axis=0)
-Ymean, Ystd = Y.mean(axis=0), Y.std(axis=0)
+# print(X.shape, ',', Y.shape, ',', P.shape)
 
-j = 31
-w = ((60*2)//10)
+xMean = np.fromfile('./data/Xmean.bin', dtype=np.float32)
+xStd = np.fromfile('./data/Xstd.bin', dtype=np.float32)
+yMean = np.fromfile('./data/Ymean.bin', dtype=np.float32)
+yStd = np.fromfile('./data/Ystd.bin', dtype=np.float32)
 
-Xstd[w*0:w * 1] = Xstd[w*0:w * 1].mean()  # Trajectory Past Positions
-Xstd[w*1:w * 2] = Xstd[w*1:w * 2].mean()  # Trajectory Future Positions
-Xstd[w*2:w * 3] = Xstd[w*2:w * 3].mean()  # Trajectory Past Directions
-Xstd[w*3:w * 4] = Xstd[w*3:w * 4].mean()  # Trajectory Future Directions
-Xstd[w*4:w*10] = Xstd[w*4:w*10].mean()  # Trajectory Gait
-
-# Mask Out Unused Joints in Input
-
-joint_weights = np.array([
-    1,
-    1e-10, 1, 1, 1, 1,
-    1e-10, 1, 1, 1, 1,
-    1e-10, 1, 1,
-    1e-10, 1, 1,
-    1e-10, 1, 1, 1, 1e-10, 1e-10, 1e-10,
-    1e-10, 1, 1, 1, 1e-10, 1e-10, 1e-10]).repeat(3)
-
-Xstd[w*10+j*3*0:w*10+j*3*1] = Xstd[w*10+j*3*0:w *
-                                   10+j*3*1].mean() / (joint_weights * 0.1)  # Pos
-Xstd[w*10+j*3*1:w*10+j*3*2] = Xstd[w*10+j*3*1:w *
-                                   10+j*3*2].mean() / (joint_weights * 0.1)  # Vel
-Xstd[w*10+j*3*2:] = Xstd[w*10+j*3*2:].mean()  # Terrain
-
-Ystd[0:2] = Ystd[0:2].mean()  # Translational Velocity
-Ystd[2:3] = Ystd[2:3].mean()  # Rotational Velocity
-Ystd[3:4] = Ystd[3:4].mean()  # Change in Phase
-Ystd[4:8] = Ystd[4:8].mean()  # Contacts
-
-Ystd[8+w*0:8+w*1] = Ystd[8+w*0:8+w*1].mean()  # Trajectory Future Positions
-Ystd[8+w*1:8+w*2] = Ystd[8+w*1:8+w*2].mean()  # Trajectory Future Directions
-
-Xmean.astype(np.float32).tofile('./result/Xmean.bin')
-Ymean.astype(np.float32).tofile('./result/Ymean.bin')
-Xstd.astype(np.float32).tofile('./result/Xstd.bin')
-Ystd.astype(np.float32).tofile('./result/Ystd.bin')
+# print(xMean.shape)
+# print(xStd.shape)
+# print(yMean.shape)
+# print(yStd.shape)
 
 # Normalize Data
+X = (X - xMean) / xStd
+Y = (Y - yMean) / yStd
 
-X = (X - Xmean) / Xstd
-Y = (Y - Ymean) / Ystd
-
-
+# Slice Input Data
+DATA_LENGTH = xMean.shape[0]
+features_batch, labels_batch =\
+    tf.train.batch((X, Y), batch_size=BATCH_SIZE, num_threads=8)
 # Layer
 
+features = tf.placeholder(tf.float32, [1, 342])
+labels = tf.placeholder(tf.float32, [1, 311])
 
+W0 = tf.Variable(tf.truncated_normal([342, 512]))
+b0 = tf.Variable(tf.constant(0, shape=[512]))
+W1 = tf.Variable(tf.truncated_normal([512, 512]))
+b1 = tf.Variable(tf.constant(0, shape=[512]))
+W2 = tf.Variable(tf.truncated_normal([512, 311]))
+b2 = tf.Variable(tf.constant(0, shape=[311]))
+
+network = tf.nn.elu(
+    W2 * tf.nn.elu(
+        W1 * tf.nn.elu(
+            W0 * features + b0) + b1) + b2)
+
+loss = tf.reduce_min(tf.square(network - labels))
+
+with tf.Session() as sess:
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    sess.run(tf.global_variables_initializer())
+    for e in range(EPOCH):
+        feed_dict = {features: features_batch, labels: labels_batch}
+        sess.run(feed_dict=feed_dict)
+        pass
+    coord.request_stop()
+    coord.join(threads)
+
+tf.train.Saver.save(network)
+
+
+# Incremental Learning
---
+++ Plan.md
@@ -0,0 +1,8 @@
+1. BVH 动画文件读取和浏览器
+2. BVH 骨骼到 UnityChan 模型的绑定
+3. Unity 上神经网络库（优先支持 CPU）
+4. 使用 UnityChan 模型预览 PFNN 预测结果
+5. 实现 IK
+6. 使用 Tensorflow 重写 PFNN
+7. 引入增量学习机制
+8. 改进相位函数
